e5b283a01acfbe6e1a8dcc37f1cf0115531a2d32
Markov654 <iliq_markov@abv.bg>
2024-07-17 23:00:31 +0300

    mytask

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Добре дошли!
-        </p>
-    
-      </header>
-    </div>
-  );
+
+
+class App extends Component{
+  render() {
+    return(
+         <div className="App">
+          <Header title ='Furst' />
+          <Content color = "violet">This is mql bay fall</Content>
+          <Content color = "green">This is mql bay fall</Content>
+          <Content color = "blue">This is mql bay fall</Content>
+          <Content color = "broun">This is mql bay fall</Content>
+          <Footer></Footer>
+        </div>
+    );
+  }
+}
+
+class Header extends Component{
+  render() {
+    return(
+      <div className="App">
+      <h1> My React App {this.props.title}</h1>
+      <p> This is my mission to write react</p>
+      </div>
+    );
+  }
+}
+
+class Content extends Component{
+  render(){
+    return(
+       <div style={{color: this.props.color}}>
+        <i>{this.props.children}</i>
+       </div>
+    );
+  }
+}
+
+class Footer extends Component{
+  render(){
+    return(
+      <>
+      <br></br>
+      <hr></hr>
+      <small>This is evethig friends</small>
+      </>
+    );
+  }
 }
 
 export default App;
